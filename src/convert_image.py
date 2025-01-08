f004def7cbfe6d38464fc86c7b9f0871c5336faa
dds100 <dds101@alumnos.unican.es>
2025-01-09 00:57:33 +0100

    Guardado automático de distintos tamaños

--- src/convert_image.py
+++ src/convert_image.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Lectura de imagen
-image_path = 'eiffel_tower.jpg'
+image_path = 'images\eiffel_tower.jpg'
 image = cv2.imread(image_path)
 
 # A escala de grises
@@ -13,11 +13,12 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 _, binary = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV)
 
 # Reshape a más pequeña
-output = cv2.resize(binary, None, fx=.3, fy=.3)
+output = cv2.resize(binary, None, fx=.15, fy=.15)
 
 # # Mostrar imagen
 # plt.imshow(output, cmap='gray')
 # plt.show()
 
 # Guardar imagen
-cv2.imwrite('output.jpg', output)
+height, width = output.shape[:2]
+cv2.imwrite(f'images\output_{height}x{width}.jpg', output)
