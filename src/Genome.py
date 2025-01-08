ab15e1f88c1adbeb60d7a100a8e08a37bd71af7f
dds100 <dds101@alumnos.unican.es>
2025-01-08 19:59:05 +0100

    Implementado método de reproducción en Genoma y constructor a partir de iterable

--- src/Genome.py
+++ src/Genome.py
@@ -5,15 +5,20 @@ import numpy as np
 
 class Genome:
 
-    def __init__(self, genome_size):
-        self.genome_size = genome_size
-        self.genome = self.initialize_genome(genome_size)
+    def __init__(self, brute_genome=None, genome_size=None):
+        if brute_genome is not None:
+            self.genome = np.array(brute_genome)
+            if len(self.genome.shape) == 1: self.genome_size = self.genome.shape[0]
+            else: self.genome_size = self.genome.shape[0] * self.genome.shape[1]
+        else:
+            self.genome_size = genome_size
+            self.genome = self.initialize_random_genome(genome_size)
 
     def __repr__(self):
         return f'Genome({self.genome})'
 
-    def initialize_genome(self, genome_size):
-        return np.random.randint(low=0, high=2, size=(1, genome_size), dtype=np.int8)
+    def initialize_random_genome(self, genome_size):
+        return np.random.randint(low=0, high=2, size=(genome_size,), dtype=np.int8)
 
     def get_image_score(self, image: np.ndarray) -> int:
         height, width = image.shape[:2]
@@ -22,7 +27,20 @@ class Genome:
         common_pixels = cv2.bitwise_and(genome_copy, image)
         return np.count_nonzero(common_pixels)
     
-
+    def reproduce(self, another_genome: "Genome", mutation_rate: float = .1) -> "Genome":
+        child_genome = []
+        for i in range(self.genome_size):
+            random_number = np.random.random()
+            if random_number < mutation_rate:
+                new_gen = np.random.randint(low=0, high=2)
+            elif random_number > 1 - (1 - mutation_rate)/2:
+                new_gen = self.genome[i]
+            else:
+                new_gen = another_genome.genome[i]
+            child_genome.append(new_gen)
+        return Genome(child_genome)
+        
+    
 class Generation:
 
     def __init__(self,
@@ -31,7 +49,7 @@ class Generation:
         ):
         self.population_size = population_size
         self.mutation_rate = mutation_rate
-        self.population = self.initialize_poputlation()
+        self.population = self.initialize_population()
 
     def initialize_population(self):
         population = []
@@ -41,5 +59,7 @@ class Generation:
 
 if __name__ == '__main__':
 
-    genome = Genome()
+    brute_genome = np.array([0, 1, 1, 0])
+    # genome = Genome(brute_genome)
+    genome = Genome(genome_size=10)
     print(genome)
--- src/reproduction.py
+++ src/reproduction.py
@@ -6,30 +6,14 @@ from Genome import Genome
 
 '''Implementación de diferentes algoritmos de reproducción.'''
 
-genome_size = 100
-genome1 = Genome(genome_size).genome
-genome2 = Genome(genome_size).genome
+genome_size = 10
+genome1 = Genome(genome_size=genome_size)
+genome2 = Genome(genome_size=genome_size)
 
 print(genome1)
 print(genome2)
 
-child_genome = []
-reasons = []
-mutation_rate = .1
-for i in range(genome_size):
-    random_number = np.random.random()
-    if random_number < mutation_rate:
-        new_gen = np.random.randint(low=0, high=2)
-        reason = 'mutation'
-    elif random_number > 1 - (1 - mutation_rate)/2:
-        new_gen = genome1[0][i]
-        reason = 'genome1'
-    else:
-        new_gen = genome2[0][i]
-        reason = 'genome2'
-    child_genome.append(new_gen)
-    reasons.append(reason)
+child_genome = genome1.reproduce(genome2)
 
 print('Child genome:', child_genome)
 print('Child genome:', type(child_genome))
-print('Reasons:', reasons)
