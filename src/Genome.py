e7fa5f8675b70d8298695c78516576ada91c21b1
dds100 <dds101@alumnos.unican.es>
2025-01-08 22:47:49 +0100

    Implementación de lógica de generación en main y correción de bug en Genome

--- src/Genome.py
+++ src/Genome.py
@@ -22,7 +22,7 @@ class Genome:
 
     def get_image_score(self, image: np.ndarray) -> int:
         height, width = image.shape[:2]
-        genome_copy = self.genome
+        genome_copy = self.genome.astype(np.int8).copy()
         genome_copy.resize((height, width))
         common_pixels = cv2.bitwise_and(genome_copy, image)
         return np.count_nonzero(common_pixels)
@@ -39,22 +39,6 @@ class Genome:
                 new_gen = another_genome.genome[i]
             child_genome.append(new_gen)
         return Genome(child_genome)
-        
-    
-class Generation:
-
-    def __init__(self,
-            population_size: int,
-            mutation_rate: float = .5
-        ):
-        self.population_size = population_size
-        self.mutation_rate = mutation_rate
-        self.population = self.initialize_population()
-
-    def initialize_population(self):
-        population = []
-        for _ in range(self.population_size):
-            population.append(Genome())
 
 
 if __name__ == '__main__':
--- src/main.py
+++ src/main.py
@@ -14,13 +14,42 @@ image = image / 255
 image = image.astype(np.int8)
 height, width = image.shape
 
-# Generar una posible solucion
+# Generar una población de soluciones
 genome_size = height * width
-genome = Genome(genome_size)
+population_size = 100
+population = []
+for i in range(population_size):
+    genome = Genome(genome_size=genome_size)
+    population.append(genome)
+print('Población de soluciones:', population)
 
-# Convertirla a tamaño de la imagen original
-score = genome.get_image_score(image)
-print(score)
+# Medir la puntuación de cada solución
+scores = []
+for genome in population:
+    score = genome.get_image_score(image)
+    scores.append(score)
+print('Scores:', scores)
+
+# Ordenar la población para obtener los dos mejores genomas
+population_scores = zip(population, scores)
+population_scores = sorted(population_scores, key=lambda item: -item[1])
+best_items = population_scores[:2]
+print('Dos mejores genomas:', best_items)
+
+# Cruzar esos dos genomas para crear una nueva generación
+population = []
+for i in range(population_size):
+    genome = best_items[0][0].reproduce(best_items[1][0])
+    population.append(genome)
+print('Generación nueva:', population)
+
+# Medir la puntuación de cada solución
+scores = []
+for genome in population:
+    score = genome.get_image_score(image)
+    scores.append(score)
+print('Scores:', scores)
+
 
 # # Mostrar genoma
 # plt.imshow(genome, cmap='gray')
