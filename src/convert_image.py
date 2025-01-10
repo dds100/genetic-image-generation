a2124b867795bbd390b71e25d1fdaabde33d5e59
dds100 <dds101@alumnos.unican.es>
2025-01-10 01:51:34 +0100

    Cambiadas imágenes

--- src/convert_image.py
+++ src/convert_image.py
@@ -3,20 +3,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Lectura de imagen
-image_path = 'images\eiffel_tower.jpg'
+# image_path = 'images\eiffel_tower.jpg'
+image_path = 'images\pikachu.png'
 image = cv2.imread(image_path)
 
 # A escala de grises
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Umbralizado
-_, binary = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV)
+_, binary = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
 
-# Reshape a más pequeña
-output = cv2.resize(binary, None, fx=.15, fy=.15)
+# # Reshape a más pequeña
+# output = cv2.resize(binary, None, fx=.05, fy=.05)
+output = binary
 
 # # Mostrar imagen
-# plt.imshow(output, cmap='gray')
+# plt.imshow(gray, cmap='gray')
 # plt.show()
 
 # Guardar imagen
